5e701e343a2601e985213f325b02f8bcc7dab287
coytar <coytar@outlook.com>
2024-03-04 04:39:52 +1100

    Updated with latest packages. Added babelrc file. Removed old stylelint rules. Eslint will warn about unused vars. Uses local-web-server for backend proxy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
 /* eslint-disable no-undef */
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
-    plugins: [new ESLintPlugin()],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
-    }
+  entry: "./src/js/index.js",
+  plugins: [new ESLintPlugin()],
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
 };
